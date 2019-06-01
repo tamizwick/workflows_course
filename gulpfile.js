9437622ca8d96fa14d0a6254ac970e48c6da9721
Tami Zwick <tami.zwick@yahoo.com>
2019-05-31 21:12:09 -0500

    Concatenated JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 
 gulp.task('coffee', function (done) {
     gulp.src(coffeeSources)
@@ -13,3 +20,10 @@ gulp.task('coffee', function (done) {
         .pipe(gulp.dest('components/scripts'));
     done();
 })
+
+gulp.task('js', function (done) {
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'));
+    done();
+});
