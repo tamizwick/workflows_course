5bdf8896b31718a8c45684acd444b83f2198a270
Tami Zwick <tami.zwick@yahoo.com>
2019-06-12 21:00:08 -0500

    Minified HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify-es').default;
+    uglify = require('gulp-uglify-es').default,
+    minifyHTML = require('gulp-minify-html');
 
 var env,
     coffeeSources,
@@ -75,7 +76,7 @@ gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
-    gulp.watch(htmlSources, ['html']);
+    gulp.watch('builds/development/*.html', ['html']);
     gulp.watch(jsonSources, ['json']);
 });
 
@@ -89,7 +90,9 @@ gulp.task('connect', function () {
 });
 
 gulp.task('html', function () {
-    gulp.src(htmlSources)
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHTML()))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
         .pipe(connect.reload());
 });
 
